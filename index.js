5d5e5062121cd0854f18fd8828b5fa8b1bc1c773
Linda <86129384+Linda-NL@users.noreply.github.com>
2021-07-21 21:10:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// defining the function 'makePizza'
+const makePizza = function () {
+    console.log('MAKE PIZZA');
+    console.log('Go to the supermarket.');
+    console.log('Think of what kind of pizza you want.');
+    console.log('Pick your pizza and buy it.');
+    console.log('Go home and pre-heat your oven');
+    console.log('Put your pizza in the pre-heated oven and put the timer on.');
+    console.log('Have a little patience.');
+    console.log('Bleep. Take your pizza out of the oven.');
+    console.log('Put your pizza on a plate and slice it.');
+    console.log('Your pizza is ready to consume. Enjoy!');
+}
+
+/*
+// calling the function 'makePizza'
+makePizza();
+
+// calling the function 3 times
+makePizza();
+makePizza();
+makePizza();
+*/
+
+// defining the function 'makeSushi'
+const makeSushi = function () {
+    console.log('MAKE SUSHI');
+    console.log('I have no idea how to make sushi.');
+    console.log('I know you need sushi rice and some rice paper.');
+    console.log('I recommend sushi with salmon, mango, cucumber, avocado or shrimps.')
+    console.log('But you better order it somewhere.');
+    console.log('In Eindhoven you can order very nice sushi at SushiExpress or at SushiPoint.');
+}
+
+/*
+//calling the function 'makeSushi'
+makeSushi();
+*/
+
+// changing output to: 3x sushi, 2x pizza
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
